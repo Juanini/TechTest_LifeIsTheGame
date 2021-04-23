ac5dd6e45adcac24658644e6a378150174361157
Juan Vazquez <diclonius32@gmail.com>
2021-04-23 10:39:08 -0500

    * Unity Project Setup

---
+++ Assets/Core/Scripts/Managers/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LifeIsTheGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+    }
+}
