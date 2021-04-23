d2ce3700399706f47d28e45704fbb1af64cb7b67
Juan Vazquez <diclonius32@gmail.com>
2021-04-23 11:22:15 -0500

    * [Project] Events Manager Added

---
+++ Assets/Core/Scripts/Events/GameEventParam.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LifeIsTheGame
+{
+    public class GameEventParam
+    {
+        public static string SCREEN_TO_OPEN = "SCREEN_TO_OPEN";
+    }
+}
---
+++ Assets/Core/Scripts/Events/GameEvents.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LifeIsTheGame
+{
+    public class GameEvents
+    {
+        public static string E_OPEN_SCREEN = "E_OPEN_SCREEN";
+
+        public static string FPS_TOGGLE = "FPS_TOGGLE";
+    }
+}
