7fab69174ab69f929f114dbf62ff6b75fabcaf1d
Juan Vazquez <diclonius32@gmail.com>
2021-04-23 19:52:54 -0500

    * [SelectMenu] Loop animation.

--- Assets/Core/Scripts/UI/Buttons/DanceSelectButton.cs
+++ Assets/Core/Scripts/UI/Buttons/DanceSelectButton.cs
@@ -1,15 +1,20 @@
+using TMPro;
 using GameEventSystem;
 using Sirenix.OdinInspector;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Shapes;
 
 namespace LifeIsTheGame
 {
     public class DanceSelectButton : MonoBehaviour
     {   
         private Button button;
+        private Image buttonImage;
+        public TextMeshProUGUI buttonText;
+        public Rectangle rectShapes;
 
         [ValueDropdown("DanceTypeOptions")] public int danceType;
 
@@ -17,12 +22,33 @@ namespace LifeIsTheGame
         {
             button = GetComponent<Button>();
             button.onClick.AddListener(OnDanceSelect);
+
+            SetupButton();
+        }
+
+        private void SetupButton()
+        {
+            switch(danceType)
+            {
+                case GameConstants.DANCE_TYPE_HOUSE:
+                buttonText.text = "House Dance";
+                break;
+
+                case GameConstants.DANCE_TYPE_MACARENA:
+                buttonText.text = "Macarena!";
+                break;
+
+                case GameConstants.DANCE_TYPE_HIP_HOP:
+                buttonText.text = "Hip Hop";
+                break;
+
+            }
         }
 
         private void OnDanceSelect()
         {
             Trace.Log("Dance Selected - Type: " + danceType);
-            
+
             Hashtable ht = new Hashtable();
             ht.Add(GameEventParam.DANCE_TYPE, danceType);
             GameEventManager.TriggerEvent(GameEvents.E_DANCE_SELECTED, ht);
@@ -30,7 +56,19 @@ namespace LifeIsTheGame
         
         private void SetSelected(bool _selected)
         {
+            if(_selected)
+            {
+                rectShapes.gameObject.SetActive(true);
+            }
+            else
+            {
+                rectShapes.gameObject.SetActive(false);
+            }
+        }
 
+        private void PlaySelectAnim()
+        {
+            
         }
 
         private static IEnumerable DanceTypeOptions = new ValueDropdownList<int>()
